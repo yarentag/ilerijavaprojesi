2649206cf7d165aa80238022dd0cf4c5fb9abc26
yarentag <yarentag23@gmail.com>
2025-01-09 13:55:59 +0300

    User entity sınıfı eklendi. Kullanıcı bilgileri için id ve username alanları tanımlandı.

--- src/main/java/com/example/ilerijava/entity/User.java
+++ src/main/java/com/example/ilerijava/entity/User.java
@@ -1,4 +1,32 @@
 package com.example.ilerijava.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+    // Getter ve Setter Metotları
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+}
+
 }
