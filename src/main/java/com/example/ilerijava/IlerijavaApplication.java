6b73edb753249c88098a532abe43928425094d65
yarentag <yarentag23@gmail.com>
2025-01-09 13:42:13 +0300

    proje oluşturuldu

---
+++ src/main/java/com/example/ilerijava/IlerijavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ilerijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlerijavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlerijavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ilerijava/IlerijavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ilerijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlerijavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
