2594249e621d49eaa22d25e0535c135af8f6dfd1
yarentag <yarentag23@gmail.com>
2025-01-09 13:51:53 +0300

    HelloService sınıfı eklendi. Servis katmanında "Merhaba, Spring Boot!" mesajını döndüren metot oluşturuldu. ``

--- src/main/java/com/example/ilerijava/service/HelloService.java
+++ src/main/java/com/example/ilerijava/service/HelloService.java
@@ -1,4 +1,12 @@
 package com.example.ilerijava.service;
 
+import org.springframework.stereotype.Service;
+@Service
+
 public class HelloService {
+
+    public String getHelloMessage() {
+        return "Merhaba, Spring Boot!";
+}
+
 }
