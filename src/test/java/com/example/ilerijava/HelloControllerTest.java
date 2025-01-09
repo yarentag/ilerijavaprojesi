c573cb467092c97e935a3db66e11aad89b251b17
yarentag <yarentag23@gmail.com>
2025-01-09 14:43:00 +0300

    hellocontroller test sınıfı oluşturuldu

---
+++ src/test/java/com/example/ilerijava/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.ilerijava;
+
+import com.example.ilerijava.controller.HelloController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSayHello() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Merhaba, Spring Boot!"));
+    }
+}
+
